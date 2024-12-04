0e0827a6018ca2c45fc0d4b1602576216f2b31bd
jkennethdomingo <johnkennethnickodomingo@gmail.com>
2024-12-04 19:55:20 +0800

    Update Pusher.php

--- app/Config/Pusher.php
+++ app/Config/Pusher.php
@@ -6,9 +6,9 @@ use CodeIgniter\Config\BaseConfig;
 
 class Pusher extends BaseConfig
 {
-    public $appId     = '1905529';
-    public $key       = '423a1b2b7b3786d99280';
-    public $secret    = '5ec9ff98dc84fcaadd94';
+    public $appId     = '1905553';
+    public $key       = '63b3aef700906222c623';
+    public $secret    = 'f5fc0b2e94159dd4388b';
     public $cluster   = 'ap1';
     public $encrypted = true;
 }
