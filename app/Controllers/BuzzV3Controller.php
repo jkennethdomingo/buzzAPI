aa94a2d58f6dd36a658637b2c4de11034eda1bc2
patrick333c <jdmalaluan2@gmail.com>
2024-12-09 13:46:52 +0800

    Update BuzzV3Controller.php

--- app/Controllers/BuzzV3Controller.php
+++ app/Controllers/BuzzV3Controller.php
@@ -604,7 +604,7 @@ class BuzzV3Controller extends ResourceController
                 return $this->respond([
                     'status' => true,
                     'message' => 'Help request submitted successfully.',
-                    'requires_help' => $updatedActivity['requires_help'], // Return the updated value of is_done
+                    'requ_help' => $updatedActivity['requ_help'], // Return the updated value of is_done
 
                 ], ResponseInterface::HTTP_OK);
             } else {
@@ -660,7 +660,7 @@ class BuzzV3Controller extends ResourceController
                 return $this->respond([
                     'status' => true,
                     'message' => 'Help request canceled successfully.',
-                    'requires_help' => $updatedActivity['requires_help'], // Return the updated value of is_done
+                    'requ_help' => $updatedActivity['requ_help'], // Return the updated value of is_done
 
                 ], ResponseInterface::HTTP_OK);
             } else {
